fdfb1db182ed376c59dd0af721e002830d8da3bb
Syaqir Syamsul <syaqir.syamsul@datumcorp.com>
2025-01-12 20:39:42 +0800

    get time n price

--- lib/pages/train_select.dart
+++ lib/pages/train_select.dart
@@ -1,6 +1,8 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:trainbook/pages/coach_select.dart';
-import 'package:trainbook/pages/ticket.dart';
+import 'package:trainbook/ticket.dart';
 
 class TrainSelectionPage extends StatefulWidget {
   final Ticket ticket;
@@ -12,6 +14,45 @@ class TrainSelectionPage extends StatefulWidget {
 
 class _TrainSelectionPageState extends State<TrainSelectionPage> {
   final List<String> _trains = ['Train A', 'Train B', 'Train C', 'Train D'];
+  String departureDT = '';
+
+  double getPrice() {
+    final random = Random();
+
+    double price = widget.ticket.price;
+    double differ = 0.90 + random.nextDouble() * 0.10;
+    price *= differ;
+    price = double.parse(price.toStringAsFixed(1));
+
+    return price;
+  }
+
+  String getTime() {
+    DateTime date = DateTime.parse(widget.ticket.date1);
+    double duration = widget.ticket.duration; 
+    
+    final random = Random();
+    
+    int randomHours = random.nextInt(24); 
+    int randomMinutes = random.nextInt(60); 
+    
+    DateTime departure = DateTime(date.year, date.month, date.day, randomHours, randomMinutes);
+
+    departureDT = departure.toUtc().toIso8601String();
+    
+    DateTime arrival = departure.add(Duration(hours: duration.toInt()));
+    
+    String formattedDeparture = '${departure.hour.toString().padLeft(2, '0')}:${departure.minute.toString().padLeft(2, '0')}';
+    String formattedArrival = '${arrival.hour.toString().padLeft(2, '0')}:${arrival.minute.toString().padLeft(2, '0')}';
+    
+    return '$formattedDeparture - $formattedArrival';
+  }
+
+
+  @override
+  void initState() {
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -33,18 +74,29 @@ class _TrainSelectionPageState extends State<TrainSelectionPage> {
               child: ListView.builder(
                 itemCount: _trains.length,
                 itemBuilder: (context, index) {
+                  double price = getPrice();
+                  String time = getTime();
                   return Card(
                     margin: const EdgeInsets.symmetric(vertical: 8),
                     child: ListTile(
-                      title: Text(_trains[index]),
-                      trailing: const Icon(Icons.arrow_forward),
+                      title: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(_trains[index]),
+                          Text(time),
+                        ],
+                      ),
+                      trailing: Text('RM ${price.toStringAsFixed(2)}'),
                       onTap: () {
                         widget.ticket.train =  _trains[index];
+                        widget.ticket.price = price;
+                        widget.ticket.date1 = departureDT;
                         Navigator.push(
                           context,
                           MaterialPageRoute(
                             builder: (context) => CoachSelectionPage(
                               ticket: widget.ticket,
+                              time: time
                             ),
                           ),
                         );
